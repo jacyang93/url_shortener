58bc0fed0f7988fabae35d31523924b1ca5723fd
Jacy <Jacyang93@gmail.com>
2017-05-25 14:24:05 +0800

    define shorten method and and vailidation for Url

--- app/models/url.rb
+++ app/models/url.rb
@@ -1,2 +1,12 @@
 class Url < ApplicationRecord
+  validates :long_url, uniqueness: true
+  before_save :shorten
+
+  def shorten
+    self.short_url = SecureRandom.hex(3)
+  end
+
+  def increase
+    self.count  += 1
+  end
 end
